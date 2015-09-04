9fab7bb650800eec885bfcae3198a2d31a309537
Johan Löfstrand <johan.lofstrand@svt.se>
2015-09-04 17:07:11 +0200

    first version

---
+++ Assets/BulletController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+	public GameObject explosionPrefab;
+	
+	void OnCollisionEnter(Collision collision) {
+		Instantiate (explosionPrefab, transform.position, transform.rotation);
+		Destroy (this);
+	}
+}
---
+++ Assets/Rifle/RifleController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RifleController : MonoBehaviour {
+	
+	public GameObject barrel;
+	public GameObject shellHole;
+	public Rigidbody bulletPrefab;
+	public Rigidbody shellPrefab;
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Rigidbody bullet = (Rigidbody) Instantiate(bulletPrefab,
+			                                           barrel.transform.position,
+			                                           transform.rotation);
+			bullet.transform.rotation *= Quaternion.Euler(-90, 0, 0);
+			bullet.velocity = -transform.forward * 100;
+			
+			Rigidbody shell = (Rigidbody) Instantiate(shellPrefab,
+			                                          shellHole.transform.position,
+			                                          transform.rotation);
+			shell.transform.rotation *= Quaternion.Euler(-90, 0, 0);
+			shell.velocity = transform.up * 3;
+			shell.angularVelocity = new Vector3 (10, 10, 10);
+		}
+	}
+}
